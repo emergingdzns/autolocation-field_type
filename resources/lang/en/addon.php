b19d33429eb5623de7c0459fd91e3519180f585f
Andrew Christensen <andrew@emergingdesigns.com>
2018-11-09 13:46:51 -0800

    initial commit

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Autolocation',
+    'name'        => 'Autolocation Field Type',
+    'description' => 'Google Place Autocomplete location entry'
+];
---
+++ tests/Feature/AutolocationFieldTypeTest.php
@@ -0,0 +1,10 @@
+<?php namespace Emergingdzns\AutolocationFieldType\Test\Feature;
+
+class AutolocationFieldTypeTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
